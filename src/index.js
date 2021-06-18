26b957f46d17608a9f6d96a52bf3b34ef57bf660
Luis Carlos <luiscarlosbguedes@gmail.com>
2021-06-17 22:28:42 -0300

    Frontend Web com React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./index.css";
+
+function CardItem(props) {
+  return (
+    <Route
+      render={({ history }) => (
+        <div
+          className="card_item"
+          onClick={() => {
+            history.push("/visualizar/" + props.item._id);
+          }}
+        >
+          <h2>{props.item.nome}</h2>
+          <img src={props.item.imagem} alt={props.item.nome} width="300" />
+        </div>
+      )}
+    />
+  );
+}
+
+class ListarItens extends React.Component {
+  // 1 - Definir estado inicial
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      itens: [],
+    };
+  }
+
+  async componentDidMount() {
+    const request = await fetch("https://backend-flexivel.herokuapp.com/", {
+      headers: new Headers({
+        Authorization: "lcbguedes",
+      }),
+    });
+
+    const json = await request.json();
+
+    // 2 - Atualiza o estado
+    this.setState({
+      itens: json,
+    });
+  }
+
+  render() {
+    // 3 - Renderiza utilizando a informação que está no estado
+    return (
+      <div className="lista_itens">
+        {this.state.itens.map((item, index) => (
+          <CardItem item={item} key={index} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class VisualizarItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.id = this.props.match.params.id;
+
+    this.state = {
+      item: {},
+    };
+  }
+
+  async componentDidMount() {
+    const request = await fetch(
+      "https://backend-flexivel.herokuapp.com/" + this.id,
+      {
+        headers: new Headers({
+          Authorization: "lcbguedes",
+        }),
+      }
+    );
+
+    const json = await request.json();
+
+    // 2 - Atualiza o estado
+    this.setState({
+      item: json,
+    });
+  }
+
+  render() {
+    return (
+      <div className="card_item">
+        <h2>{this.state.item.nome}</h2>
+        <img
+          src={this.state.item.imagem}
+          alt={this.state.item.nome}
+          width="300"
+        />
+        <div>{this.state.item.descricao}</div>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" exact={true} component={ListarItens} />
+
+      <Route path="/visualizar/:id" component={VisualizarItem} />
+    </Switch>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
